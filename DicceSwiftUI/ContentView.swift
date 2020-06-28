76cbfdcfe939b5351bdfd5a4b64e2149f231107b
Tejasree V <tejasree.vangapelli@gmail.com>
2020-06-27 18:27:57 -0700

    Initial Commit

---
+++ DicceSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DicceSwiftUI
+//
+//  Created by tejasree vangapalli on 6/27/20.
+//  Copyright © 2020 tejasree vangapalli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
